5e1e9701df0a45082d1eccb2a94638414c96e829
landx <lawdetaildev@gmail.com>
2025-03-30 18:53:04 -0400

    syn2

--- code/userprog/pcbmanager.h
+++ code/userprog/pcbmanager.h
@@ -1,12 +1,9 @@
 #ifndef PCBMANAGER_H
 #define PCBMANAGER_H
 
-<<<<<<< HEAD
 #include "bitmap.h"
 #include "pcb.h"
 
-=======
->>>>>>> 9ebf9d60684a6d62655ae7944013a2acf924bb67
 class PCBManager{
 
     public:
@@ -18,12 +15,8 @@ class PCBManager{
 
     private:
         Bitmap* bitmap;
-<<<<<<< HEAD
         PCB** pcbs;
         Lock* pcbManagerLock;     
-=======
-        PCB** pcbs;     
->>>>>>> 9ebf9d60684a6d62655ae7944013a2acf924bb67
 }
 
 #endif
