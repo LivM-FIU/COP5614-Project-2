c76930def94b43b1b65821f16796e6ce1e8187a5
dtalawdetail <dtalawdetail@lawdetail.com>
2025-04-04 00:47:54 -0400

    aaaaa

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -296,7 +296,7 @@ int doKill(int pid)
 void doYield()
 {
     DEBUG('t', "System Call: [%d] invoked Yield.\n", currentThread->space->pcb->pid);
-    printf('t', "System Call: [%d] invoked Yield.\n", currentThread->space->pcb->pid);
+    printf("System Call: [%d] invoked Yield.\n", currentThread->space->pcb->pid);
     currentThread->Yield();
 }
 
