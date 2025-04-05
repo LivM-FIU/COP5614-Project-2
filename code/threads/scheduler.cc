c19870fc2ecaaceca203cd6dd4fe35c1719325e4
dtalawdetail <dtalawdetail@lawdetail.com>
2025-04-05 18:02:27 -0400

    reverse last

--- code/threads/scheduler.cc
+++ code/threads/scheduler.cc
@@ -127,6 +127,12 @@ Scheduler::Run (Thread *nextThread)
 	threadToBeDestroyed = NULL;
     }
     
+#ifdef USER_PROGRAM
+    if (currentThread->space != NULL) {		// if there is an address space
+        currentThread->RestoreUserState();     // to restore, do it.
+	currentThread->space->RestoreState();
+    }
+#endif
 }
 
 //----------------------------------------------------------------------
