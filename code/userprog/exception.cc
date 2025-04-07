9013e9c4ae39fcdb151a870182eeaa282a741b16
dtalawdetail <dtalawdetail@lawdetail.com>
2025-04-06 20:21:37 -0400

    sasa

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -167,7 +167,7 @@ int doFork(int functionAddr)
 
 int doExec(char *filename)
 {
-    printf("System Call: [%d] invoked \n", currentThread->space->pcb->pid);
+    printf("System Call: [%d] invoked Exec\n", currentThread->space->pcb->pid);
 
     // 1. Open the executable file
     OpenFile *executable = fileSystem->Open(filename);
